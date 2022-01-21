39f148a6fdae758190e0c8fc937e56a90416d52f
martinbolsnes <martinbolsnes@icloud.com>
2022-01-21 15:37:29 +0100

    added select styled comp

---
+++ components/input/select.js
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+const Select = styled.select`
+  height: 30px;
+  width: 300px;
+  border-radius: 5px;
+  border: black 2px solid;
+  color: black;
+`;
+
+export default Select;
