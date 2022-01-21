51c5651d403e8f0666e9d7d14c9bfecdb0c397be
martinbolsnes <martinbolsnes@icloud.com>
2022-01-21 15:37:14 +0100

    added input styled comp

---
+++ components/input/input.js
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+const Input = styled.input`
+  height: 30px;
+  width: 300px;
+  border-radius: 5px;
+  border: black 2px solid;
+  color: black;
+`;
+
+export default Input;
