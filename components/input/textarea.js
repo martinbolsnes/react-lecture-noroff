d81cdd8cb8c3ac6ab1bee58eec7430f37a93641c
martinbolsnes <martinbolsnes@icloud.com>
2022-01-21 15:37:42 +0100

    added texarea styled comp

---
+++ components/input/textarea.js
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+
+const Textarea = styled.textarea`
+  height: 100px;
+  width: 300px;
+  border-radius: 5px;
+  border: black 2px solid;
+`;
+
+export default Textarea;
