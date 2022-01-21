33b7a6422a93b4f78055f91ccac1e58ce8f4f4df
martinbolsnes <martinbolsnes@icloud.com>
2022-01-21 15:36:50 +0100

    added reset button comp

---
+++ components/button/resetButton.js
@@ -0,0 +1,15 @@
+import styled from 'styled-components';
+
+const ResetButton = styled.button`
+  background-color: black;
+  height: 50px;
+  width: 150px;
+  color: white;
+  appearance: none;
+  border: none;
+  border-radius: 5px;
+  margin-top: 10px;
+  margin-bottom: 10px;
+`;
+
+export default ResetButton;
